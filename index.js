30cc56dbb3dfe037da4e031fb501ba8552ea005a
Neysel <neyseltheblack@gmail.com>
2025-12-14 22:24:21 +0400

    fix changing theme item

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ themeButton.addEventListener('click', function() {
         document.body.style.backgroundColor = '#1e1f27';
         document.documentElement.style.setProperty('--text', 'white')
         document.documentElement.style.setProperty('--pink', '#ee2db4')
-        // footerImg.style.filter = 'invert(100%)'
+     code_example_wrapper.style.backgroundColor = '#2d2d3a'
         themeButton.style.backgroundImage = 'url(../rsschool-cv/assets/icons/light_mode.png)'
         themeButton.style.filter = 'invert(100%)'
 
@@ -16,7 +16,7 @@ themeButton.addEventListener('click', function() {
         document.body.style.backgroundColor = '#fafafa';
         document.documentElement.style.setProperty('--text', 'black')
         document.documentElement.style.setProperty('--pink', '#5c0042')
-        // footerImg.style.filter = 'invert(0%)'
+        code_example_wrapper.style.backgroundColor = 'white'
         themeButton.style.backgroundImage = 'url(../rsschool-cv/assets/icons/night_mode.png)'
         themeButton.style.filter = 'invert(0%)'
 
